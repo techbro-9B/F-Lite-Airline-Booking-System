b84c812cd4ced4293ae8b447cfdf204b312de5ea
Lloyd Tendo <tendolloyd@gmail.com>
2026-02-09 17:33:00 -0500

    Made a few changes. - updated metadata - added the basic sections needed in a landing page. Sections of the landing page are saved in  / app/homepage/components

---
+++ frontend/src/app/homepage/components/WhereWeFly.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const WhereWeFly
+ = () => {
+  return (
+    <div>
+        WhereWeFly... everywhere!    
+    </div>
+  )
+}
+
+
+export default WhereWeFly
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,20 @@
 import Image from "next/image";
 import HeroSection from "./homepage/components/HeroSection";
 import { HomeNavBar } from "./homepage/components/HomeNavBar";
+import AboutUs from "./homepage/components/AboutUs";
+import WhereWeFly from "./homepage/components/WhereWeFly";
+import Footer from "./homepage/components/Footer";
 export default function Home() {
   return (
     <>
+    {/* The basic stuff we need for a landing page... */}
     <HomeNavBar/>
 
-    <HeroSection/> 
-    
+    <HeroSection/>
+    <AboutUs/>
+    <WhereWeFly/> 
+    <Footer/>
+      
     </>
   );
 }
