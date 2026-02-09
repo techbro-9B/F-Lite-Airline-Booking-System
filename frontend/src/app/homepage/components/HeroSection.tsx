7f40d7539ecb28b3b392c8052f42a3b037a94891
Lloyd Tendo <tendolloyd@gmail.com>
2026-02-09 17:17:16 -0500

    Added a navbar to the homepage. Created custom component, MaxWidthWrapper.tsx for general use around the application so that there's consistency in UI

---
+++ frontend/src/app/homepage/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+import { MaxWidthWrapper } from '@/components/MaxWidthWrapper'
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <MaxWidthWrapper>
+        Hero Section!
+    </MaxWidthWrapper>
+  )
+}
+
+export default HeroSection
