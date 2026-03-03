49fa2fb8614e1c86d2a49c4ba624852ee26504a5
Lloyd Tendo <tendolloyd@gmail.com>
2026-03-02 23:39:52 -0500

    made a supabase folder in lib/supabase
    you need to create .env.local in your root this is where the supabase keys will be held for safety, not in .ts or .tsx files. The .env file won't show in commits because it's ignored in .gitignore
    
    for the client.ts & server.ts, jst copy past what I made in the feature/userauth branch... don't need to rewrite EVERything!

---
+++ frontend/src/lib/supabase/client.ts
@@ -0,0 +1,7 @@
+/* 
+ This is where the clientSide supabase object will be stored.
+
+Created by Lloyd, march 2, 2026
+updated: Lloyd, march 2, 2026 
+
+*/
---
+++ frontend/src/lib/supabase/server.ts
@@ -0,0 +1,9 @@
+/* 
+ This is where the ServerSide supabase object will be stored.
+
+Created by Lloyd, march 2, 2026
+updated: Lloyd, march 2, 2026 
+
+you will have to download missing dependencies: npm install
+
+*/
